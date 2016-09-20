d8d07d24d8603a655dacd48cf99e3547ab6078d5
zhurlik <uladzimir_zhuraulevich@epam.com>
2016-09-20 19:10:57 +0300

    added few tasks

--- src/main/javascript/pages/main.jsx
+++ src/main/javascript/pages/main.jsx
@@ -5,24 +5,45 @@ var css = require('../css/main.css');
 module.exports = function () {
     var Block = React.createClass({
 
-        task1: function(event) {
-            console.log('11111111');
+        getInitialState: function() {
+            return {
+                taskDescription:'Задание №1: Вопрос для первого задания',
+                taskHandler: this.task1,
+                taskVideo: require('../video/intro.mp4')
+            }
         },
 
+        task1: function(event) {
+            console.log('11111111');
+            this.setState({
+                taskDescription: 'Задание №2: Вопрос для второго задания',
+                taskHandler: this.task2,
+                taskVideo: require('../video/task2.mp4')
+            })
+        },
+
+        task2: function(event) {
+            console.log('22222222');
+            this.setState({
+                taskDescription: 'Задание №2: Вопрос для третьего задания',
+                taskHandler: this.task2,
+                taskVideo: require('../video/task3.mp4')
+            })
+        },
 
         render: function() {
             return (
                 <div id="slide">
                     <video>
-                        <source src={require('../video/intro.mp4')} type="video/mp4" controls/>
+                        <source src={this.state.taskVideo} type="video/mp4" controls/>
                         Ваш браузер не поддерживает видео.
                     </video>
                     <div id="question">
                         <div>
-                            Задание№ 1: 111111111 12312 31234234 234 234 234 234
+                            {this.state.taskDescription}
                         </div>
                         <label>Ответ:</label><input type="text"/>
-                        <button onClick={this.task1}>Дальше</button>
+                        <button onClick={this.state.taskHandler}>Дальше</button>
                     </div>
                 </div>
             );
