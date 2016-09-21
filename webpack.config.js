3ddf15895ceb82ba5ae56a33d17f3caf0450ca63
zhurlik <uladzimir_zhuraulevich@epam.com>
2016-09-21 13:41:30 +0300

    added fonts

--- webpack.config.js
+++ webpack.config.js
@@ -25,8 +25,7 @@ module.exports = {
         }),
         new webpack.ProvidePlugin({
             // to be able to use $ in any modules
-            $: "jquery",
-            Backbone : "backbone"
+            $: "jquery"
         })
     ],
 
@@ -42,7 +41,11 @@ module.exports = {
             },
             { test: /\.css$/, loader: "style-loader!css-loader" },
             { test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
-            {test: /\.mp4$|\.jpg$|\.png$/, loader: 'url?limit=1000&mimetype=video/mp4'}
+            // images and video
+            {test: /\.mp4$/, loader: 'url?limit=1000&mimetype=video/mp4&name=../video/[name].[ext]'},
+            {test: /\.jpg$|\.png$/, loader: 'url?limit=1000&mimetype=image/jpeg&name=../img/[name].[ext]'},
+            // Font Definitions
+            { test: /\.[ot]tf$/, loader: 'url?limit=1000&mimetype=application/octet-stream&name=../font/[name].[ext]' }
         ]
     }
 };
