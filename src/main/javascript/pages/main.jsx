2716f164d1def439b7616afe506a51d9ef9c7700
zhurlik <uladzimir_zhuraulevich@epam.com>
2016-09-24 15:42:13 +0300

    added more tasks and messages

--- src/main/javascript/pages/main.jsx
+++ src/main/javascript/pages/main.jsx
@@ -32,6 +32,7 @@ module.exports = function () {
             console.log('>> Переходим к заданиям...');
 
             this.setState({
+                pageId: 'task1',
                 showVideo: 'show',
                 showQuestion: 'notShow',
                 showInput: 'show',
@@ -67,6 +68,8 @@ module.exports = function () {
 
             // right code
             if (this.refs.answer.value === '111') {
+
+                // a few messages before going to next task
                 this.setState({
                     showInput: 'notShow',
                     showButton: 'notShow'
@@ -74,13 +77,23 @@ module.exports = function () {
                 this.sayError('А вот и фигушки!!!', 200);
                 this.sayError('Ладно, ладно. Шучу :)', 2000);
 
+                // go to task2
                 var goToNext = function() {
                     this.setState({
+                        pageId: 'task2',
                         showVideo: 'show',
                         showQuestion: 'notShow',
+                        taskDescription: 'Как Вы уже слышали, после крушения Морган спасся на необитаемом острове. ' +
+                        'В этом задании тебе нужно найти послание, которое Морган отправил в надежде, что нашедший послание спасет его. ' +
+                        'А как ты знаешь, все пираты попавшие на необитаемый остов, отправляют свои послание в запечатанных бутылках. ' +
+                        'Найди бутылку в самом морском месте квартиры, и Морган на бутылке поведает тебе свой секрет. ' +
+                        'В ответ на это задание Вы должены сообщить мне координаты острова, на котором Морган закопал свой клад.',
                         taskHandler: this.task2,
-                        taskVideo: require('../video/task2.mp4')
+                        taskVideo: require('../video/task2.mp4'),
+                        button: 'Попытка не пытка'
                     });
+                    // clean
+                    this.refs.answer.value = '';
                 }.bind(this);
 
                 setTimeout(function() {
@@ -100,26 +113,111 @@ module.exports = function () {
             }
         },
 
+        /**
+         * Checks entered answer for second task.
+         *
+         * @param event
+         */
         task2: function (event) {
             console.log('>> Отвечаем на второе задание и пытаемся перейти к следующему заданию');
-            this.setState({
-                showVideo: 'show',
-                showQuestion: 'notShow',
-                taskDescription: 'Задание №3: необходимо ответить на вопрос 3333',
-                taskHandler: this.task3,
-                taskVideo: require('../video/task3.mp4')
-            })
+
+            // right code
+            if (this.refs.answer.value === '222') {
+
+                // a few messages before going to next task
+                this.setState({
+                    showInput: 'notShow',
+                    showButton: 'notShow'
+                });
+                this.sayError('Пук-пук-пук!!!', 200);
+                this.sayError('Так уж и быть идем дальше', 2000);
+
+                // go to task3
+                var goToNext = function() {
+                    this.setState({
+                        pageId: 'task3',
+                        showVideo: 'show',
+                        showQuestion: 'notShow',
+                        taskDescription: 'Если Вы настоящие пираты, то должны знать, что среди пиратов существует негласное соревнование «за чью поимку запросят большее вознаграждение». ' +
+                        'Такие объявления обычно вешают в тавернах, где люди едят и всегда могут обратить внимание. ' +
+                        'Мое задание: найди у себя объявление о моем розыске, сумму за мою поимку умножь на количество якорей в бухте мертвеца и прибавь количество флагов на приглашении, ' +
+                        'внизу которого нарисованы ЯДРО, ШТУРВАЛ, ЧЕРЕП',
+                        taskHandler: this.task3,
+                        taskVideo: require('../video/task3.mp4'),
+                        button: 'Не промахнись'
+                    });
+                    // clean
+                    this.refs.answer.value = '';
+                }.bind(this);
+
+                setTimeout(function() {
+                    this.setState({
+                        taskDescription: 'Отлично, теперь я смогу найти сокровища Моргана, спасибо Вам, Вы оказались более полезеными, чем я ожидал. ' +
+                        'Часть карты находится в большой синей книге у вас в комнате. Йо-Хо-Хо и бутылка рома!!!',
+                        taskHandler: goToNext,
+                        button: 'Погнали',
+                        showButton: 'show',
+                        error: ''
+                    });
+                }.bind(this), 5000);
+            } else {
+                this.setState({
+                    error: 'Ой-Ой, снова ручки-крючки, думайте'
+                });
+            }
         },
 
         task3: function (event) {
             console.log('>> Отвечаем на третье задание и пытаемся перейти к следующему заданию');
-            this.setState({
-                showVideo: 'show',
-                showQuestion: 'notShow',
-                taskDescription: 'Задание №4: необходимо ответить на вопрос 4444',
-                taskHandler: this.task4,
-                taskVideo: require('../video/task4.mp4')
-            })
+
+            // right code
+            if (this.refs.answer.value === '333') {
+
+                // a few messages before going to next task
+                this.setState({
+                    showInput: 'notShow',
+                    showButton: 'notShow'
+                });
+                this.sayError('Пук-пук-пук!!!', 200);
+                this.sayError('Так уж и быть идем дальше', 2000);
+
+                // go to task4
+                var goToNext = function() {
+                    this.setState({
+                        pageId: 'task4',
+                        showVideo: 'show',
+                        showQuestion: 'notShow',
+                        taskDescription: 'Пираты очень суеверный народ, и когда я был на африканском континенте, одна гадалка ВУДУ под страхом смерит сказала мне, ' +
+                        'что я обрету величайшую силу, когда стану обладателем заклятия. В своем видении она увидела, что заклинание это было выбито на неком предмете ' +
+                        'круглой формы темного цвета, на одной из сторон предмета изображена смерть, а на другой стороне написано само заклятие. ' +
+                        'Конечно, откуда старой гадалке из глухой деревни знать, что она описала в точности черную метку, и именно ту, ' +
+                        'которую мне вручили на корабле «Летучего голландца». ' +
+                        'Узнайте заклятие и сообщите его мне, взамен я расскажу, где спрятана часть карты. ' +
+                        'Ах, да, чуть не забыл, гадалка сказала, что видит этот предмет в темном месте. ' +
+                        'К этому месту можно пройти от книжного шкафа семь шагов к двери и 14 шагов к спальне. Сам не пойму о чем идет речь, но думаю, Вы решишь это задание, ведь части карты еще у меня!',
+                        taskHandler: this.task4,
+                        taskVideo: require('../video/task4.mp4'),
+                        button: 'Не заблудитесь'
+                    });
+                    // clean
+                    this.refs.answer.value = '';
+                }.bind(this);
+
+                setTimeout(function() {
+                    this.setState({
+                        taskDescription: 'Ух, ты! Вы все правильно посчитали, а мы-то думали, никто никогда не сможет пройти это задание. ' +
+                        'Пираты привыкли полагаться на свою силу, а не на ум. Часть карты спрятана в стиральной машинке. Буль-Буль-Буль.',
+                        taskHandler: goToNext,
+                        button: 'Понеслась',
+                        showButton: 'show',
+                        error: ''
+                    });
+                }.bind(this), 5000);
+            } else {
+                this.setState({
+                    error: 'Включи мозги, если они у тебя есть. Ха-ха-ха'
+                });
+            }
         },
 
 
@@ -164,6 +262,11 @@ module.exports = function () {
                 showVideo: 'notShow',
                 showQuestion: 'show'
             });
+            if (this.state.pageId === 'task2' || this.state.pageId === 'task3') {
+                this.setState({
+                   showInput: 'show'
+                });
+            }
         },
 
         videoHandler: function (video) {
